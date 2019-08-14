eb6ae298abefc9530ab539984653ff9581618873
Apple-yong <748840808@qq.com>
2019-08-14 18:19:18 +0800

    关于我的页面赋值

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,66 +1,25 @@
 // pages/me/me.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tab: "tomato",
+    lists: {
+      "本周四":[
+        {time: "14:20", text: "回家回家军或或或或", id:1},
+        { time: "14:20", text: "回家回家军或或或或", id: 1 },
+        { time: "14:20", text: "回家回家军或或或或", id: 1 }
+      ],
+      "本周五": [{ time: "14:20", text: "回家回家军或或或或", id: 1 }],
+      "本周六": [{ time: "14:20", text: "回家回家军或或或或", id: 2 }],
+      "本周五": [{ time: "14:20", text: "回家回家军或或或或", id: 3 }],
+      "本周五": [{ time: "14:20", text: "回家回家军或或或或", id: 4 }],
+      "本周五": [{ time: "14:20", text: "回家回家军或或或或", id: 5 }]
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  changeTab(event){
+    let name = event.currentTarget.dataset.name
+    this.setData({ tab: name })
   }
 })
