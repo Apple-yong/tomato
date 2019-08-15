356a1b2bad3e8912ad1381a8e1a3f602652ff7ff
Apple-yong <748840808@qq.com>
2019-08-15 16:34:20 +0800

    登录及删除app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',
-    app_id: "wx729448e4aeeccd0b",
-    app_secret: "45f9810412de63addaf25b6a9281f3be",
-    t_app_id: "hVLCXU42vZ5JhPVkw6Br46CK",
-    t_app_secret: "LczWBwdVg9Et2pFpM7HyAA81"
-  }
-})
--- pages/login/login.js
+++ pages/login/login.js
@@ -5,6 +5,31 @@ Page({
 
   },
   onShow(){
-    http.get('/todos')
+    
+  },
+  login(e){
+    let code;
+    let iv = e.detail.iv;
+    let encrypted_data = e.detail.encryptedData;
+    wx.login({
+      success: (res) => this.loginMe(res.code, iv, encrypted_data)
+    })
+  },
+  loginMe(code, iv, encrypted_data) {
+    http.post('/sign_in/mini_program_user', {
+      code,
+      iv,
+      encrypted_data,
+      app_id,
+      app_secret,
+    })
+      .then(response => {
+        this.saveMessage(response)
+        wx.reLaunch({ url: "/pages/home/home" })
+      })
+  },
+  saveMessage(response) {
+    wx.setStorageSync('me', response.data.resource)
+    wx.setStorageSync('X-token', response.header["X-token"])
   }
 })
